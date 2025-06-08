50a0bc0430127940f213c7f98d093715349bd9f8
kipli-rice-cooker <11241077@student.itk.ac.id>
2025-06-08 17:42:28 +0800

    Add files via upload

---
+++ laman_login/Laman_login.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laman_login;
+
+/**
+ *
+ * @author LENOVO LOQ
+ */
+public class Laman_login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login lg = new login ();
+        lg.setVisible (true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(register.EXIT_ON_CLOSE);
+    }
+    
+}
